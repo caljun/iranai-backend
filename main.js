37db840d1c9c1bbb49209ab10bc82292e29ca80f
caljun <calderonjunya0602@gmail.com>
2025-04-06 01:14:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB接続成功"))
